5777d8ce61ba966845633793bc469117f48f0a2d
Tiron <ole_g92@bk.ru>
2021-11-18 20:12:11 +0300

    second

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -11,7 +11,7 @@
         <input type="text" placeholder="Введите ссылку">
         <h6 class="main__fist-column-price">Цена товара</h6>
         <input type="text" placeholder="Введите цену">
-        <button class="button" >Добавить товар</button>
+        <button class="button" @click="say('hi')">Добавить товар</button>
       </div>
     </div>
     <div class="main__second-column">
@@ -41,8 +41,10 @@
 
 .main__second-column {
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  flex-wrap: wrap;
   height: 100%;
+  gap: 16px;
   width: 100%;
   border: 1px solid red;
 }
@@ -129,17 +131,60 @@ letter-spacing: -0.02em;
 color: #49485E;
 }
 
+.main__second-column-card{
+  width: 332px;
+  height: 423px;
+  border: 1px solid red;
+}
+
+.main__second-column-card-img {
+  width: 100%;
+  height: 200px;
+}
+
+.main__second-column-card-description {
+  width: 300px;
+  height: 80px;
+}
+
+
 
 </style>
 
 
 
 <script>
-// export default {
-//   methods: {
-//     say: function (message) {
-//       alert(message)
-//     }
-//   }
-// }
+export default {
+  methods: {
+    say: function (message) {
+      const cards = document.querySelector('.main__second-column');
+      const card = document.createElement('div');
+      const img = document.createElement('img');
+      const title = document.createElement('h3');
+      const description = document.createElement('p');
+      const price = document.createElement('p');
+
+
+
+
+      card.classList.add('main__second-column-card');
+      img.classList.add('main__second-column-card-img');
+      title.classList.add('main__second-column-card-title');
+      description.classList.add('main__second-column-card-description');
+      price.classList.add('main__second-column-card-price');
+
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/240px-Cat03.jpg';
+    title.textContent = 'lolololol';
+    description.textContent = 'lolololollololol ollolololollolololollololololl olololollolololollolololol';
+    price.textContent = '100500';
+
+      alert(message);
+      card.appendChild(img);
+      card.appendChild(title);
+      card.appendChild(description);
+      card.appendChild(price);
+      cards.appendChild(card);
+    }
+  }
+}
 </script>
